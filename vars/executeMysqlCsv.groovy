4b1059dbbdf9467f0bf794c18f945a739e06bf7f
Ravi Shekhar <ravishekhar.thakur@visionwaves.com>
2026-02-17 15:43:23 +0530

    Add parameterized decode stage to shared library

--- vars/executeMysqlCsv.groovy
+++ vars/executeMysqlCsv.groovy
@@ -1,4 +1,34 @@
-def call() {
-    echo "Shared library from Ravi personal repo loaded ✅"
+def call(Map config = [:]) {
+
+    if (!config.csvFile?.trim()) {
+        error "CSV Base64 missing"
+    }
+
+    if (!config.clusterName) {
+        error "Cluster name is required"
+    }
+
+    if (!config.schemaName) {
+        error "Schema name is required"
+    }
+
+    if (!config.env) {
+        error "Environment is required"
+    }
+
+    stage('Decode CSV') {
+
+        echo "Environment: ${config.env}"
+        echo "Cluster: ${config.clusterName}"
+        echo "Schema: ${config.schemaName}"
+
+        writeFile file: "input.b64", text: config.csvFile
+
+        sh """
+            cat input.b64 | base64 -d > uploaded.csv
+        """
+
+        echo "✔ CSV decoded successfully"
+    }
 }
 
